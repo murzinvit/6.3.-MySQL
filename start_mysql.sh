d0459de02eed045299565dc482c542e0019a90fe
Vitaliy Murzin <61565909+murzinvit@users.noreply.github.com>
2021-09-04 18:03:01 +0500

    Update start_mysql.sh

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop mysrv
 docker rm mysrv
-docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass -v ~/mysql-data:/var/lib/mysql --name mysrv mysql:latest sleep 600000000
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -v /DATABASE/mysql-data:/var/lib/mysql --name mysrv mysql:8
